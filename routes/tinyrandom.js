23c026c7d238b0c0e2251ec12409acab91085047
Daniel Neumann <dkneuma@gmail.com>
2015-06-26 20:29:27 -0400

    Fixed TinyRandom
    TinyRandom now returns 1 random verse based on this formula:
     Tinyverse.find(null,null,{limit: -1, skip : Math.random() *
    versecount},function (err, tinyverses) {
    
    where versecount is a global variable that records number of verses in
    the DB

--- routes/tinyrandom.js
+++ routes/tinyrandom.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var router = express.Router();
+var versecount = 0;
 
 var mongoose = require('mongoose');
 var Tinyverse = require('../models/TinyVerse.js');
@@ -11,7 +12,20 @@ var Tinyverse = require('../models/TinyVerse.js');
 router.get('/', function(req, res, next) {
     console.log("GET called");
 
-    Tinyverse.find(function (err, tinyverses) {
+    // First, get count of records
+    Tinyverse.count(function (err, count) {
+        if (err) return next(err);
+        console.log('there are %d tiny verses', count);
+        versecount = count;
+    });
+
+    /* Then figure out how to do this:
+
+     Tinyverse.find().limit(-1).skip(Math.random() * versecount);
+
+     */
+    Tinyverse.find(null,null,{limit: -1, skip : Math.random() * versecount},function (err, tinyverses) {
+        console.log('there are STILL %d tiny verses', versecount);
         if (err) return next(err);
         res.json(tinyverses);
     });
